3ea4dd6d9f34dac6ccb5897aebfe899a63b510ae
GitYeJiaWei <y994471>
2018-11-07 18:07:55 +0800

    java 基础代码提交5

---
+++ src/com/list/AsLiat.java
@@ -0,0 +1,114 @@
+package com.list;
+
+import java.util.*;
+
+/**
+ * 数组转换成集合
+ * 数组转换成集合虽然不能增加或减少元素,但是可以用集合的思想操作数组,也就是说可以使用其他集合中的方法
+ */
+public class AsLiat {
+    public static void main(String[] args){
+        //集合转数组,加泛型的
+        ArrayList<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        list.add("d");
+
+        String[] arr = list.toArray(new String[10]);		//当集合转换数组时,数组长度如果是小于等于集合的size时,转换后的数组长度等于集合的size
+        //如果数组的长度大于了size,分配的数组长度就和你指定的长度一样
+        for (String string : arr) {
+            System.out.println(string);
+        }
+
+        demo2();
+        demo3();
+        demo4();
+        demo5();
+        demo6();
+    }
+
+    public static void demo2() {
+        //int[] arr = {11,22,33,44,55};
+        //List<int[]> list = Arrays.asList(arr);			基本数据类型的数组转换成集合,会将整个数组当作一个对象转换
+        //System.out.println(list);
+        Integer[] arr = {11,22,33,44,55};					//将数组转换成集合,数组必须是引用数据类型
+        List<Integer> list = Arrays.asList(arr);
+        System.out.println(list);
+    }
+
+    public static void demo3() {
+        String[] arr ={"asdf","asd","ads","ads"};
+        List<String> list = Arrays.asList(arr);
+        System.out.println(list);
+    }
+
+    public static void demo4(){
+        ArrayList list = new ArrayList();				//创建集合对象
+        list.add(new Srudent(12,"张三"));
+        list.add(new Srudent(12,"张三"));
+        list.add(new Srudent(34,"李四"));
+        list.add(new Srudent(34,"李四"));
+
+
+        ArrayList newList = getSingle(list);			//调用方法去除重复
+        System.out.println(newList.size());
+
+        list.remove(new Srudent(34,"李四"));
+        System.out.println(list.size());
+
+    }
+
+    /*
+     * 创建新集合将重复元素去掉
+     * 1,明确返回值类型,返回ArrayList
+     * 2,明确参数列表ArrayList
+     *
+     * 分析:
+     * 1,创建新集合
+     * 2,根据传入的集合(老集合)获取迭代器
+     * 3,遍历老集合
+     * 4,通过新集合判断是否包含老集合中的元素,如果包含就不添加,如果不包含就添加
+     */
+    public static ArrayList getSingle(ArrayList list) {
+        ArrayList newList = new ArrayList<>();					//1,创建新集合
+        Iterator it = list.iterator();							//2,根据传入的集合(老集合)获取迭代器
+
+        while(it.hasNext()) {									//3,遍历老集合
+            Object obj = it.next();								//记录住每一个元素
+            if(!newList.contains(obj)) {						//如果新集合中不包含老集合中的元素
+                newList.add(obj);								//将该元素添加
+            }
+        }
+
+        return newList;
+    }
+
+    public static void demo5(){
+        LinkedList list = new LinkedList();
+        list.addFirst("a");
+        list.addFirst("b");
+        list.addFirst("c");
+        list.addFirst("d");
+        list.addLast("e");
+
+        //System.out.println(list.getFirst());
+        //System.out.println(list.getLast());
+        //System.out.println(list.removeFirst());
+        //System.out.println(list.removeLast());
+
+        System.out.println(list.get(1));
+        System.out.println(list);          /*dcbae*/
+
+    }
+
+    public static void demo6(){
+        Stack s = new Stack();
+        s.push("a");                  //进栈
+        s.push("b");
+
+        while (!s.empty()){                 //判断栈结构是否为空
+            System.out.println(s.pop());    //弹栈
+        }
+    }
+}
--- src/com/list/Srudent.java
+++ src/com/list/Srudent.java
@@ -1,5 +1,8 @@
 package com.list;
 
+
+import com.view.Equals;
+
 public class Srudent {
     private int age;
     private String name;
@@ -24,4 +27,48 @@ public class Srudent {
     public void setName(String name) {
         this.name = name;
     }
+
+    //底层比较的是地址，重写该方法比较其内容
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)						//调用的对象和传入的对象是同一个对象
+            return true;						//直接返回true
+        if (obj == null)						//传入的对象为null
+            return false;						//返回false
+        if (getClass() != obj.getClass())		//判断两个对象对应的字节码文件是否是同一个字节码
+            return false;						//如果不是直接返回false
+        Srudent other = (Srudent) obj;			//向下转型
+        if (age != other.age)					//调用对象的年龄不等于传入对象的年龄
+            return false;						//返回false
+        if (name == null) {						//调用对象的姓名为null
+            if (other.name != null)				//传入对象的姓名不为null
+                return false;					//返回false
+        } else if (!name.equals(other.name))	//调用对象的姓名不等于传入对象的姓名
+            return false;						//返回false
+        return true;							//返回true
+    }
+
+    @Override
+    public String toString() {
+        return "name:"+this.name+" age:"+this.age;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+   /* @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Srudent){
+            Srudent srudent = (Srudent) obj;
+            if (this.name.equals(srudent.name) && this.age ==srudent.age){
+                return true;
+            }else {
+                return false;
+            }
+        }else {
+            return false;
+        }
+    }*/
 }
